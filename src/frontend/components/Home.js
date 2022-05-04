5d554fdd4b26f433d5b8b1581e75e83bc174731b
Romain TREFAULT <romain.trefault@epita.fr>
2022-05-04 13:12:31 -0400

    Home front end

---
+++ src/frontend/components/Home.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
+import { Row, Col, Card, Button } from 'react-bootstrap'
+
+const Home = ({ marketplace, nft }) => {
+  const [loading, setLoading] = useState(true)
+  const [items, setItems] = useState([])
+  const loadMarketplaceItems = async () => {
+    // Load all unsold items
+    const itemCount = await marketplace.itemCount()
+    let items = []
+    for (let i = 1; i <= itemCount; i++) {
+      const item = await marketplace.idToItem(i)
+      if (!item.sold) {
+        // get uri url from nft contract
+        const URI = await nft.tokenURI(item.tokenID)
+        // use uri to fetch the nft metadata stored on ipfs 
+        const response = await fetch(URI)
+        const metadata = await response.json()
+        // get total price of item (item price + fee)
+        const totalPrice = await marketplace.getTotalPrice(item.itemID)
+        // Add item to items array
+        items.push({
+          totalPrice,
+          itemID: item.itemID,
+          seller: item.seller,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image
+        })
+      }
+    }
+    setLoading(false)
+    setItems(items)
+  }
+
+  const buyMarketItem = async (item) => {
+    await (await marketplace.purchaseItem(item.itemID, { value: item.totalPrice })).wait()
+    loadMarketplaceItems()
+  }
+
+  useEffect(() => {
+    loadMarketplaceItems()
+  }, [])
+  if (loading) return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>No items</h2>
+    </main>
+  )
+  return (
+    <div className="flex justify-center">
+      {items.length > 0 ?
+        <div className="px-5 container">
+          <Row xs={1} md={2} lg={4} className="g-4 py-5">
+            {items.map((item, idx) => (
+              <Col key={idx} className="overflow-hidden">
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Body color="primary">
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <div className='d-grid'>
+                      <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
+                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
+                      </Button>
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+        : (
+          <main style={{ padding: "1rem 0" }}>
+            <h2>No listed assets</h2>
+          </main>
+        )}
+    </div>
+  );
+}
+export default Home
