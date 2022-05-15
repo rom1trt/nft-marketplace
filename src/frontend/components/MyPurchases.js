fbe5abb1d430bd3b9aef2492c23271215f0b13a9
Romain TREFAULT <romain.trefault@epita.fr>
2022-05-15 13:21:29 -0400

    MyPurchases front-end

---
+++ src/frontend/components/MyPurchases.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
+import { Row, Col, Card } from 'react-bootstrap'
+
+export default function MyPurchases({ marketplace, nft, account }) {
+  const [loading, setLoading] = useState(true)
+  const [purchases, setPurchases] = useState([])
+  const loadPurchasedItems = async () => {
+    // Fetch purchased items from marketplace by quering ItemCreated events with the buyer set as the user
+    const filter =  marketplace.filters.ItemSold(null,null,null,null,null,account)
+    const results = await marketplace.queryFilter(filter)
+    //Fetch metadata of each nft and add that to listedItem object.
+    const purchases = await Promise.all(results.map(async i => {
+      // fetch arguments from each result
+      i = i.args
+      // get uri url from nft contract
+      const URI = await nft.tokenURI(i.tokenID)
+      // use uri to fetch the nft metadata stored on ipfs 
+      const response = await fetch(URI)
+      const metadata = await response.json()
+      // get total price of item (item price + fee)
+      const totalPrice = await marketplace.getTotalPrice(i.itemID)
+      // define listed item object
+      let purchasedItem = {
+        totalPrice,
+        price: i.price,
+        itemID: i.itemID,
+        name: metadata.name,
+        description: metadata.description,
+        image: metadata.image
+      }
+      return purchasedItem
+    }))
+    setLoading(false)
+    setPurchases(purchases)
+  }
+  useEffect(() => {
+    loadPurchasedItems()
+  }, [])
+  if (loading) return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>No purchases</h2>
+    </main>
+  )
+  return (
+    <div className="flex justify-center">
+      {purchases.length > 0 ?
+        <div className="px-5 container">
+          <Row xs={1} md={2} lg={4} className="g-4 py-5">
+            {purchases.map((item, idx) => (
+              <Col key={idx} className="overflow-hidden">
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Footer>{ethers.utils.formatEther(item.totalPrice)} ETH</Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+        : (
+          <main style={{ padding: "1rem 0" }}>
+            <h2>No purchases</h2>
+          </main>
+        )}
+    </div>
+  )}
