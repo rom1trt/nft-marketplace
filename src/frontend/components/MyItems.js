bf126ae3613f73be3fd6fd8b8d775eaf3305c42b
Romain TREFAULT <romain.trefault@epita.fr>
2022-06-20 20:02:21 +0200

    refactor: MyItems section

--- src/frontend/components/MyItems.js
+++ src/frontend/components/MyItems.js
@@ -2,10 +2,31 @@ import { useState, useEffect } from 'react'
 import { ethers } from "ethers"
 import { Row, Col, Card } from 'react-bootstrap'
 
+function renderSoldItems(items) {
+  return (
+    <>
+      <h2>Sold</h2>
+      <Row xs={1} md={2} lg={4} className="g-4 py-3">
+        {items.map((item, idx) => (
+          <Col key={idx} className="overflow-hidden">
+            <Card>
+              <Card.Img variant="top" src={item.image} />
+              <Card.Footer>
+               Sold For {ethers.utils.formatEther(item.totalPrice)} MATIC -
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
 export default function MyListedItems({ marketplace, nft, account }) {
   const [loading, setLoading] = useState(true)
   const [listedItems, setListedItems] = useState([])
   const [soldItems, setSoldItems] = useState([])
+
   useEffect(() => {
     const loadListedItems = async () => {
       // Load all sold items that the user listed
@@ -51,7 +72,7 @@ export default function MyListedItems({ marketplace, nft, account }) {
     <div className="flex justify-center">
       {listedItems.length > 0 ?
         <div className="px-5 py-3 container">
-            <h2>Listed & Sold</h2>
+            <h2>Listed</h2>
           <Row xs={1} md={2} lg={4} className="g-4 py-3">
             {listedItems.map((item, idx) => (
               <Col key={idx} className="overflow-hidden">
@@ -62,6 +83,7 @@ export default function MyListedItems({ marketplace, nft, account }) {
               </Col>
             ))}
           </Row>
+            {soldItems.length > 0 && renderSoldItems(soldItems)}
         </div>
         : (
           <main style={{ padding: "1rem 0" }}>
